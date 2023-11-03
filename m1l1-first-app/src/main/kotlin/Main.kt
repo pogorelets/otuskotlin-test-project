22366410361ccbe418f5877de730f6286f530a24
elenapogorelec <pogorelec@dev.vtb.ru>
2023-11-03 18:09:43 +0300

    First Homework - quickstart

--- m1l1-first-app/src/main/kotlin/Main.kt
+++ m1l1-first-app/src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+package ru.otus.otuskotlin.marketplace.m1l1
 fun main(args: Array<String>) {
     println("Hello World!")
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,10 +3,11 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-}
-
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+    plugins {
+        val kotlinVersion: String by settings
+        kotlin("jvm") version kotlinVersion
+    }
 }
 
 rootProject.name = "otuskotlin-test-project"
+include("m1l1-first-app")
